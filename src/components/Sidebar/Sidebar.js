bc2d5fffb18744551974f5f1cdfcdd8e1e035caf
Philipp Brumm <philipp.brumm@gmail.com>
2017-10-22 14:46:49 +0200

    make Sidebar groups collapsible

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -14,11 +14,42 @@ const ICON_MAP = {
   all: 'issue-opened',
   filter: 'search',
   repo: 'repo',
+  member: 'person',
 }
+
+const Group = ({ label, items, onToggle, isCollapsed }) => (
+  <Column shrink={0}>
+    <Row
+      alignItems="flex-end"
+      className={css.groupLabel}
+      shrink={0}
+      onClick={() => onToggle(label, !isCollapsed)}
+    >
+      {label}
+    </Row>
+
+    {!isCollapsed &&
+      mapObject(sortBy(items, 'result.length').reverse(), (_, { id, name, result, category }) => (
+        <NavLink to={`/${id}`} key={id} className={css.item} activeClassName={css.itemSelected}>
+          <Octicon name={ICON_MAP[category]} className={css.icon} />
+          <div className={css.itemLabel}>{name}</div>
+          {result !== null &&
+            result.length !== 0 && <div className={css.count}>{result.length}</div>}
+        </NavLink>
+      ))}
+  </Column>
+)
 
 @withRouter
 @connect(({ user }) => ({ user }))
 export default class Sidebar extends React.Component {
+  state = Object.keys(this.props.groups).reduce(
+    (state, label) => ({
+      ...state,
+      [label]: false,
+    }),
+    {}
+  )
   render() {
     const { groups, user } = this.props
 
@@ -33,28 +64,16 @@ export default class Sidebar extends React.Component {
           />
         )}
         {mapObject(groups, (groupLabel, items) => (
-          <Column shrink={0} key={groupLabel}>
-            <Row alignItems="flex-end" className={css.groupLabel} shrink={0}>
-              {groupLabel}
-            </Row>
-
-            {mapObject(
-              sortBy(items, 'result.length').reverse(),
-              (_, { id, name, result, category }) => (
-                <NavLink
-                  to={`/${id}`}
-                  key={id}
-                  className={css.item}
-                  activeClassName={css.itemSelected}
-                >
-                  <Octicon name={ICON_MAP[category]} className={css.icon} />
-                  <div className={css.itemLabel}>{name}</div>
-                  {result !== null &&
-                    result.length !== 0 && <div className={css.count}>{result.length}</div>}
-                </NavLink>
-              )
-            )}
-          </Column>
+          <Group
+            key={groupLabel}
+            label={groupLabel}
+            items={items}
+            isCollapsed={this.state[groupLabel]}
+            onToggle={(label, toggleState) =>
+              this.setState({
+                [label]: toggleState,
+              })}
+          />
         ))}
       </Column>
     )
