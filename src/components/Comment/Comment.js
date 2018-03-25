7fa3af124209d1e065055d6c047d7ab87be8c427
Philipp Brumm <philipp.brumm@gmail.com>
2018-03-25 12:51:03 +0200

    align Comment with other components that show reactions

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -13,23 +13,31 @@ const mapStateToProps = ({ entities }, { user: userId }) => ({
   user: entities.users[userId],
 })
 
-const Comment = ({ compact = false, user, body, updated_at, reactions }) => (
-  <Column className={css.container} shrink={0} style={{ marginTop: compact ? -3 : null }}>
-    {compact === false && (
-      <Row className={css.header} shrink={0} alignItems="center" grow={1}>
-        <UserBadge size={28} radius={3} avatar={user.avatar_url} />
+const Comment = ({ compact = false, user, body, updated_at, reactions }) => {
+  const hasReactions = reactions.total_count > 0
 
-        <div className={css.username}>
-          {user.login} commented <TimeAgo date={updated_at} />
-        </div>
-      </Row>
-    )}
+  return (
+    <Column
+      className={css.container}
+      shrink={0}
+      style={{ marginTop: compact ? -3 : null }}
+    >
+      {compact === false && (
+        <Row className={css.header} shrink={0} alignItems="center" grow={1}>
+          <UserBadge size={28} radius={3} avatar={user.avatar_url} />
 
-    <div className={css.commentBody}>
-      <GithubFlavoredMarkdown source={body} />
-      <Reactions reactions={reactions} />
-    </div>
-  </Column>
-)
+          <div className={css.username}>
+            {user.login} commented <TimeAgo date={updated_at} />
+          </div>
+        </Row>
+      )}
+
+      <div className={css.commentBody}>
+        <GithubFlavoredMarkdown source={body} />
+        {hasReactions && <Reactions reactions={reactions} />}
+      </div>
+    </Column>
+  )
+}
 
 export default connect(mapStateToProps)(Comment)
